c11c333390cdddeaef50cd3072fc2c89db2d4c82
juliasakakibara <juliasakakibara.o@gmail.com>
2021-07-19 15:49:45 -0300

    lab basic algorithms - missing bonus

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
-// Iteration 1: Names and Input
+// Iteraction 1
+
+let driversName = 'hacker1';
+let navigatorsName = 'hacker2';
+
+console.log(`The driver's name is ${driversName}`);
+console.log(`The navgator's name is ${navigatorsName}`);
+
+// Iteraction 2 
+if (driversName.length > navigatorsName.length){
+console.log(`The driver has the longest name, it has ${driversName.length} characters.`)
+} else if (driversName.length < navigatorsName.length){
+console.log(`It seems that the navigator has the longest name, it has ${navigatorsName.length} characters.`)
+}else {
+console.log(`Wow, you both have equally long names, ${driversName.length} characters!.`)
+};
 
 
-// Iteration 2: Conditionals
+// Iteraction 3
 
+let result = driversName.toUpperCase().split("").join(" ");
+console.log(result);
 
-// Iteration 3: Loops
+function reverseString(s){
+    return s.split("").reverse().join("");
+}
+reverseString(navigatorsName);
